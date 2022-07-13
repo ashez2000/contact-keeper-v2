98a00f33c358319e21a30661eb92f6195ddc46f0
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-07-13 12:00:01 +0530

    add app error and async handler

--- server/src/controllers/contact.controller.js
+++ server/src/controllers/contact.controller.js
@@ -1,63 +1,84 @@
 const Contact = require('../models/contact.model')
+const asyncHandler = require('../utils/async-handler.utils')
+const AppError = require('../utils/app-error.utils')
 
 /**
  * @description This function is used to get all contacts
  * @route GET /api/contacts
  */
-const getContacts = async (req, res) => {
+const getContacts = asyncHandler(async (req, res, next) => {
   const contacts = await Contact.find()
-  res.json({ contacts })
-}
+  return res.status(200).json({ status: 'success', data: { contacts } })
+})
 
 /**
  * @description This function is used to create a new contact
  * @route POST /api/contacts
  */
-const createContact = async (req, res) => {
+const createContact = asyncHandler(async (req, res, next) => {
   const { name, email, phone } = req.body
-  const newContact = new Contact({ name, email, phone })
-  await newContact.save()
-
-  return res.json({ newContact })
-}
-
-/**
- * @description This function is used to get a contact by id
- * @route GET /api/contacts/:id
- */
-const getContact = async (req, res) => {
-  const { id } = req.params
-  const contact = await Contact.findById(id)
-
-  return res.json({ contact })
-}
-
-/**
- * @description This function is used to update a contact
- * @route PUT /api/contacts/:id
- */
-const updateContact = async (req, res) => {
-  const { id } = req.params
-  const { name, email, phone } = req.body
-  const updatedContact = await Contact.findByIdAndUpdate(id, {
+  const contact = await Contact.create({
     name,
     email,
     phone,
   })
 
-  return res.json({ updatedContact })
-}
+  return res.status(201).json({ status: 'success', data: { contact } })
+})
+
+/**
+ * @description This function is used to get a contact by id
+ * @route GET /api/contacts/:id
+ */
+const getContact = asyncHandler(async (req, res, next) => {
+  const { id } = req.params
+  const contact = await Contact.findById(id)
+
+  if (!contact) {
+    return next(new AppError(`No contact found with this id:${id}`, 404))
+  }
+
+  return res.status(200).json({ status: 'success', data: { contact } })
+})
+
+/**
+ * @description This function is used to update a contact
+ * @route PUT /api/contacts/:id
+ */
+const updateContact = asyncHandler(async (req, res, next) => {
+  const { id } = req.params
+  const { name, email, phone } = req.body
+  const contact = await Contact.findByIdAndUpdate(
+    id,
+    {
+      name,
+      email,
+      phone,
+    },
+    { new: true }
+  )
+
+  if (!contact) {
+    return next(new AppError(`No contact found with this id:${id}`, 404))
+  }
+
+  return res.status(200).json({ status: 'success', data: { contact } })
+})
 
 /**
  * @description This function is used to delete a contact
  * @route DELETE /api/contacts/:id
  */
-const deleteContact = async (req, res) => {
+const deleteContact = asyncHandler(async (req, res, next) => {
   const { id } = req.params
-  await Contact.findByIdAndDelete(id)
+  const contact = await Contact.findByIdAndDelete(id)
 
-  return res.json({ message: 'Contact deleted' })
-}
+  if (!contact) {
+    return next(new AppError(`No contact found with this id:${id}`, 404))
+  }
+
+  return res.status(200).json({ status: 'success', data: { contact } })
+})
 
 module.exports = {
   getContacts,
