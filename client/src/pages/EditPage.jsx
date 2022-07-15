54a712eaa83b5129c37f617d3bf9d1e0f5b85c93
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-07-15 11:41:03 +0530

    update contact with api

--- client/src/pages/EditPage.jsx
+++ client/src/pages/EditPage.jsx
@@ -1,7 +1,81 @@
-import ContactCard from '../components/ContactCard'
+import { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
-const EditPage = () => {
-  return <ContactCard />
+const CreatePage = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+
+  const navigate = useNavigate()
+  const params = useParams()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await fetch(`/api/contacts/${params.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        phone,
+      }),
+    })
+
+    setName('')
+    setEmail('')
+    setPhone('')
+
+    navigate('/')
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`/api/contacts/${params.id}`)
+      const data = await res.json()
+      const contact = data.data.contact
+
+      setName(contact.name)
+      setEmail(contact.email)
+      setPhone(contact.phone)
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center">
+      <h1 className="mb-3">Create new contact</h1>
+      <form className="w-50" onSubmit={handleSubmit}>
+        <input
+          className="form-control mb-3"
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          className="form-control mb-3"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          className="form-control mb-3"
+          type="text"
+          placeholder="Ph. Number"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+
+        <button className="btn btn-warning">Update</button>
+      </form>
+    </div>
+  )
 }
 
-export default EditPage
+export default CreatePage
